9c9ce7df76fa715d3325f3c3fba93f42db6d3e6d
Kinasih14 <142877930+Kinasih14@users.noreply.github.com>
2024-12-13 19:46:46 +0700

    Pembuatan Petani Screen

--- navigation.js
+++ navigation.js
@@ -4,12 +4,15 @@ import { Stack } from 'expo-router';
 const Stack = createNativeStackNavigator();
 import { View, Text } from 'react-native';
 import React from 'react';
+import HomeScreen from './screens/HomeScreen';
+import PetaniScreen from './screens/PetaniScreen';
 
 export default function Navigation() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
             <Stack.Screen name='Home' component={HomeScreen} />
+            <Stack.Screen name='Petani' component={PetaniScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     )
--- screens/PetaniScreen.js
+++ screens/PetaniScreen.js
@@ -1,10 +1,10 @@
 import {View, Text} from 'react-native'
 import React from 'react'
 
-export default function HomeScreen() {
+export default function PetaniScreen() {
     return (
         <View>
-            <Text>HomeScreen</Text>
+            <Text>PetaniScreen</Text>
         </View>
     )
 }
